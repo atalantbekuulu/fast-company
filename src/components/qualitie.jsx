79bbf6a67687e6f662c5b607ce3dc10188b80064
atalantbekuulu <azamattalantbekov336@gmail.com>
2022-07-25 20:15:33 +0600

    refactoring, pagination and add PropTypes control

--- src/components/qualitie.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-const Qualitie = (props) =>{ 
-  return (<>
-     <td>{props.value.map((qualitie)=> <span className={'badge m-1 bg-'+qualitie.color} key={qualitie._id}>{qualitie.name}</span>)}</td>
-  </>)
-}
-export default Qualitie;
--- src/components/user.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-const User = (props) =>{ 
-  return (<>
-     <td>{props.value}</td>
-  </>)
-}
-export default User;
